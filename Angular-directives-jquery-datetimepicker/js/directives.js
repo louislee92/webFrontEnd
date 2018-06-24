c703cf79b0dc0a49fe1ebd74ae69d5bca6170b17
Louis Lee <louislee92@163.com>
2018-06-24 21:36:56 +0800

    test

--- Angular-directives-jquery-datetimepicker/js/directives.js
+++ Angular-directives-jquery-datetimepicker/js/directives.js
@@ -1,15 +1,4 @@
 angular.module('myApp', [])
-    .directive('myForm', function() {
-        return {
-            restrict: 'E',
-            replace: true,
-            template: '<div>当前选择时间：{{myTime}}<br><input ng-model="myTime" datetimepicker /></div>',
-            controller: ['$scope', function($scope){
-            }],
-            link: function(scope, element, attr){
-            }
-        };
-    })
     .directive('datetimepicker', function() {
         return {
             restrict: 'A',
@@ -20,15 +9,18 @@ angular.module('myApp', [])
             }],
             link: function(scope, element, attr) {
             	$.datetimepicker.setLocale('ch');  // 新版方法 $.datetimepicker.setLocale('ch');
-                element.datetimepicker({
+                console.log(attr);
+                var option = {
                     lang: "ch", //语言选择中文 注：旧版本 新版方法：$.datetimepicker.setLocale('ch');
-                    format: "Y-m-d H:i:s", //格式化日期
-                    timepicker: true, //关闭时间选项
-                    yearStart: 2000, //设置最小年份
-                    yearEnd: 2050, //设置最大年份
-                    todayButton: true, //关闭选择今天按钮
-                    step: 30, 
-                });
+                    format: attr.format || "Y-m-d", //格式化日期  Y-m-d H:i:s
+                    timepicker: attr.timepicker || (attr.format && attr.format.indexOf("H") != -1) || false, //关闭时间选项
+                    yearStart: attr.yearStart || 2000, //设置最小年份
+                    yearEnd: attr.yearStart || 2050, //设置最大年份
+                    todayButton:  attr.todayButton || true, //关闭选择今天按钮
+                    step: attr.step || 60, 
+                };
+                console.log(option);
+                element.datetimepicker(option);
             }
         }
     });
